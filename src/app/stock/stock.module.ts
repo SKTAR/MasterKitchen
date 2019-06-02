985d137a816f048ac4d3521ea63055bd32cc6459
Chailuck Chantaravisutlert <chailuck.ch@glsict.com>
2019-06-02 20:14:03 +0700

    Initial Generate Module for Stock w/ routing

---
+++ src/app/stock/stock.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { StockRoutingModule } from './stock-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    StockRoutingModule
+  ]
+})
+export class StockModule { }
